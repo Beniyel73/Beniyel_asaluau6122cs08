518d7ec8180097163e8f66a067cdce08541d8231
Beniyel73 <145739783+Beniyel73@users.noreply.github.com>
2023-09-22 10:46:55 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,9 @@
+English = 9.9
+Hindi = 8.7
+Maths = 10
+Science =9.8;  
+SocialStudy = 9
+CGPA = (9.9+8.7+10+9.8+9)/(5.0)  
+CGPAper = 9.5 * (CGPA)  
+print(" CGPA percentage is:");  
+print(CGPAper);
